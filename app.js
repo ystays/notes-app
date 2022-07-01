5fba06cf57c5881f5b39d1dcbe41350b44844b9c
ystays <85421593+ystays@users.noreply.github.com>
2022-06-30 18:11:48 -0700

    added skeleton add, remove, read, list functionality to app.js

---
+++ app.js
@@ -0,0 +1,62 @@
+//const fs = require('fs')
+const notes = require("./notes.js")
+const chalk = require('chalk')
+const yargs = require('yargs')
+
+//const msg = getNotes()
+//console.log(msg)
+
+// Customize yargs version
+yargs.version('1.0.0')
+
+// add, remove, read, list
+
+// Create add command
+yargs.command({
+    command: "add",
+    describe: "Adding a note",
+    builder: {
+        title: {
+            describe: "Note title",
+            demandOption: true,
+            type: "string"
+        },
+        body: {
+            describe: "Note body",
+            demandOption: true,
+            type: "string"
+        }
+    },
+    handler: function (argv) {
+        addNote(argv.title, argv.body)
+    }
+})
+
+
+// Create remove command
+yargs.command({
+    command: "remove",
+    describe: "Remove a note",
+    handler: function () {
+        console.log("Removing the note")
+    }
+})
+
+// Create read command
+yargs.command({
+    command: "read",
+    describe: "Read a note",
+    handler: function () {
+        console.log("Reading the note")}
+})
+
+// Create list command
+yargs.command({
+    command: "list",
+    describe: "List your notes",
+    handler: function () {
+        console.log("Listing out all notes")
+    }
+})
+
+yargs.parse()
---
+++ notes.js
@@ -0,0 +1,13 @@
+
+const getNotes = function () {
+    return "Your notes..."
+}
+
+const addNote = function (title, body) {
+    return "Adding note"
+}
+
+module.exports = {
+    getNotes: getNotes,
+    addNote: addNote
+}
